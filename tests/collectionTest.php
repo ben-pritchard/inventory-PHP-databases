b34bc961dfc8009859278a2870f1d14cd54029ee
Adam <wonton916@gmail.com>
2015-09-29 14:39:27 -0700

    add deleteAll in test file

--- tests/collectionTest.php
+++ tests/collectionTest.php
@@ -27,6 +27,25 @@
             $result = Collection::getAll();
             $this->assertEquals($test_collection, $result[0]);
         }
+
+        function  test_deleteAll()
+        {
+            //Arrange
+            $name = "Hello Kitty";
+            $name2 = "Pokemon";
+            $test_collection = new Collection($name);
+            $test_collection->save();
+            $test_collection2 = new Collection($name2);
+            $test_collection2->save();
+
+            //Act
+            Collection::deleteAll();
+            $result = Collection::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
+
+        }
     }
 
 
