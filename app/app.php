1068871e2df33256eb29d3a4825658bca31796b3
Ben <ben.s.pritchard@gmail.com>
2015-09-29 15:42:27 -0700

    Add home page view.

--- app/app.php
+++ app/app.php
@@ -1,11 +1,28 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Collection.php";
+
+    //Add symfony debug component and turn it on.
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
 
     $app = new Silex\Application();
+
+    // Set Silex debug mode in $app object
+    $app['debug'] = true;
 
     $server = 'mysql:host=localhost;dbname=inventory';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('collections' => Collection::getAll()));
+    });
+
+    return $app;
 ?>
